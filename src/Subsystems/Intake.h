be521b56d24d31bd42db71e355f3da77e6e03f11
SkylerPIlot <SkylerParthemore@gmail.com>
2017-09-23 09:05:39 -0700

    Created Intake Subsystem

---
+++ src/Subsystems/Intake.h
@@ -0,0 +1,22 @@
+#ifndef Intake_H
+#define Intake_H
+
+#include <Commands/Subsystem.h>
+
+class Intake : public frc::Subsystem {
+private:
+	// It's desirable that everything possible under private except
+	// for methods that implement subsystem capabilities
+
+	static const char kSubsystemName[];
+	static std::shared_ptr<Intake> self;
+	Intake();
+
+public:
+
+	static std::shared_ptr<Intake> getInstance();
+
+	void InitDefaultCommand();
+};
+
+#endif  // Intake_H
