2e6fe0cdb4407b928ccc6b175edcfeb7b49a1e79
SkylerPIlot <SkylerParthemore@gmail.com>
2017-09-25 22:12:41 -0700

    Finished Chassis Subsystem
    Added A function in chassis for tankdrive and created a default command for this subsystem.

---
+++ src/Commands/TankDrive.cpp
@@ -0,0 +1,39 @@
+#include "TankDrive.h"
+#include "../Subsystems/Chassis.h"
+#include "../OI.h"
+
+TankDrive::TankDrive() {
+	// Use Requires() here to declare subsystem dependencies
+	// eg. Requires(Robot::chassis.get());
+	Requires(Chassis::getInstance().get());
+}
+
+// Called just before this Command runs the first time
+void TankDrive::Initialize() {
+
+}
+
+// Called repeatedly when this Command is scheduled to run
+void TankDrive::Execute() {
+  std::shared_ptr<Chassis> chassis = Chassis::getInstance();
+  std::shared_ptr<OI> oi = OI::getInstance();
+
+	chassis->SetTankDrive(oi->kLeftYAxis_ID, oi->kRightYAxis_ID);
+
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool TankDrive::IsFinished() {
+	return false;
+}
+
+// Called once after isFinished returns true
+void TankDrive::End() {
+
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void TankDrive::Interrupted() {
+
+}
--- src/Subsystems/Chassis.h
+++ src/Subsystems/Chassis.h
@@ -2,6 +2,8 @@
 #define Chassis_H
 
 #include <Commands/Subsystem.h>
+#include "CANTalon.h"
+#include "WPIlib.h"
 
 class Chassis : public frc::Subsystem {
 
@@ -13,10 +15,21 @@ private:
 	static std::shared_ptr<Chassis> self;
 	Chassis();
 
+	CANTalon leftFrontWheel;
+	CANTalon leftRearWheel;
+	CANTalon rightFrontWheel;
+	CANTalon rightRearWheel;
+
 public:
 	static std::shared_ptr<Chassis> getInstance();
 
 	void InitDefaultCommand();
+
+	 void SetTankDrive(double left, double right);
+	  void SetCoast();
+	  void SetBrake();
+	  void SetupDrive();
+	  void SetUpTalon();
 };
 
 #endif  // Chassis_H
