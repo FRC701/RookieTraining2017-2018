c200ad358fe0cca64b303fa4fa14518e316f5101
SkylerPIlot <SkylerParthemore@gmail.com>
2017-09-26 20:38:36 -0700

    SetActuator Command
    This was to create a set command for the intake actuator

---
+++ src/Commands/SetActuator.cpp
@@ -0,0 +1,14 @@
+#include "SetActuator.h"
+
+SetActuator::SetActuator(Intake::ActuatorValue value)
+: mValue(value)
+{
+	// Use Requires() here to declare subsystem dependencies
+	// eg. Requires(Robot::chassis.get());
+	Requires(Intake::getInstance().get());
+}
+
+// Called just before this Command runs the first time
+void SetActuator::Initialize() {
+	Intake::getInstance()->SetActuator(mValue);
+}
