efd3cc3a37b5dcd8a051efbe7f8ba721282bea8f
SkylerPIlot <SkylerParthemore@gmail.com>
2017-09-28 20:14:10 -0700

    Created SetBrake Command

---
+++ src/Commands/SetBrake.cpp
@@ -0,0 +1,15 @@
+#include "SetBrake.h"
+
+SetBrake::SetBrake(Elevator::BrakeValue value)
+: mValue(value) {
+	// Use Requires() here to declare subsystem dependencies
+	// eg. Requires(Robot::chassis.get());
+	Requires(Elevator::getInstance().get());
+}
+
+// Called once when the command executes
+void SetBrake::Initialize() {
+
+	Elevator::getInstance()->SetBrake(mValue);
+
+}
