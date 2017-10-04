0e4eb4e2a2ba6b546fc0ce717c75720cf22ac6ff
tamiavalledor <tamiavalledor@gmail.com>
2017-10-04 15:48:51 -0700

    finshed

--- src/Subsystems/Chassis.cpp
+++ src/Subsystems/Chassis.cpp
@@ -1,7 +1,18 @@
 #include "Chassis.h"
 #include "../RobotMap.h"
 
-Chassis::Chassis() : Subsystem("ExampleSubsystem") {
+const char Chassis::kSubsystemName[] = "Chassis";
+
+std::shared_ptr<Chassis> Chassis::self;
+
+std::shared_ptr<Chassis> Chassis::getInstance() {
+  if (! self) {
+    self = std::shared_ptr<Chassis>(new Chassis);
+  }
+  return self;
+}
+
+Chassis::Chassis() : Subsystem(kSubsystemName) {
 
 }
 
