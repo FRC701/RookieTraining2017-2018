f14383fa910df52c11fbf130ae843bc0e2c77ece
nekster99 <nekster99@gmail.com>
2017-10-18 15:37:13 -0700

    finisseh oi.h

--- src/OI.h
+++ src/OI.h
@@ -8,15 +8,9 @@ private:
 
   static std::shared_ptr<OI> self;
 
-
-
 //..........driver..........
 
-
-
   std::shared_ptr<Joystick> driver;
-
-
 
   JoystickButton dA;
   JoystickButton dB;
@@ -30,16 +24,11 @@ private:
   JoystickButton dBack;
 
 
-//..........codriver..........
-
-
+//..........codriver.........
 
   std::shared_ptr<Joystick> coDriver;
 
-
-
   //..........Xbox..........
-
   JoystickButton coA;
   JoystickButton coB;
   JoystickButton coX;
@@ -51,19 +40,17 @@ private:
   JoystickButton coStart;
   JoystickButton coBack;
 
-
-
   //..........Custom..........
-
-
 
   JoystickButton coTurretNeg90;
   JoystickButton coTurret0;
   JoystickButton coTurret90;
   JoystickButton coTurretMinus;
   JoystickButton coTurretPlus;
+
   JoystickButton coShooterMinus;
   JoystickButton coShooterPlus;
+
   JoystickButton coShoot;
   JoystickButton coBallIntake;
   JoystickButton coGearScore;
@@ -74,33 +61,41 @@ private:
   JoystickButton coCancel;
 
 public:
-
   static std::shared_ptr<OI> getInstance();
 
 	OI();
 
 //..........driver..........
+
   std::shared_ptr<Joystick> getDriver();
+
   double getDriverLeftXAxis() const;
   double getDriverLeftYAxis() const;
+
   double getDriverRightXAxis() const;
   double getDriverRightYAxis() const;
+
   double getDriverLeftTrigger() const;
   double getDriverRightTrigger() const;
+
   void SetRumble(double amount);
 
+
 //..........codriver..........
+
   std::shared_ptr<Joystick> getCoDriver();
+
   double getCoDriverLeftXAxis() const;
   double getCoDriverLeftYAxis() const;
+
   double getCoDriverRightXAxis() const;
   double getCoDriverRightYAxis() const;
+
   double getCoDriverLeftTrigger() const;
   double getCoDriverRightTrigger() const;
 
+
 //..........ids..........
-
-
 
   //..........Xbox..........
   static const int kButtonA_ID = 1;
@@ -113,21 +108,27 @@ public:
   static const int kButtonStart_ID = 8;
   static const int kButtonL3_ID = 9;
   static const int kButtonR3_ID = 10;
+
   static const int kLeftXAxis_ID = 0;
   static const int kLeftYAxis_ID = 1;
+
   static const int kRightXAxis_ID = 4;
   static const int kRightYAxis_ID = 5;
+
   static const int kLeftTrigger_ID = 2;
   static const int kRightTrigger_ID = 3;
 
   //..........Custom..........
+
   static const int kButtonCoTurretNeg90_ID = 10;
   static const int kButtonCoTurret0_ID = 11;
   static const int kButtonCoTurret90_ID = 13;
   static const int kButtonCoTurretPlus_ID = 3;
   static const int kButtonCoTurretMinus_ID = 16;
+
   static const int kButtonCoShooterPlus_ID = 9;
   static const int kButtonCoShooterMinus_ID = 15;
+
   static const int kButtonCoShoot_ID = 5;
   static const int kButtonCoBallIntake_ID = 8;
   static const int kButtonCoGearScore_ID = 12;
@@ -136,7 +137,7 @@ public:
   static const int kButtonCoBallOuttake_ID = 1;
   static const int kButtonCoFloorOuttake_ID = 2;
   static const int kButtonCoCancel_ID = 4;
-};
 
+};
 
 #endif
