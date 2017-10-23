b07a9f2179b3dd44a25d48585ade2297a8eb0aa7
nekster99 <nekster99@gmail.com>
2017-10-23 16:01:47 -0700

    intake done

---
+++ src/Subsystems/Intake.cpp
@@ -0,0 +1,25 @@
+#include "Intake.h"
+#include "../RobotMap.h"
+
+const char Intake::kSubsystemName[] = "Intake";
+
+std::shared_ptr<Intake> Intake::self;
+
+std::shared_ptr<Intake> Intake::getInstance() {
+  if (! self) {
+     self = std::shared_ptr<Intake>(new Intake);
+  }
+  return self;
+}
+
+Intake::Intake() : Subsystem(kSubsystemName) {
+
+}
+
+void Intake::InitDefaultCommand() {
+	// Set the default command for a subsystem here.
+	// SetDefaultCommand(new MySpecialCommand());
+}
+
+// Put methods for controlling this subsystem
+// here. Call these from Commands
