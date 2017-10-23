36ba9aa2b14e3a193a39a6e46c333dcdd63be305
trinitydeloviar <trinityd@vandenrobotics.com>
2017-10-23 16:52:27 -0700

    commit

--- src/Subsystems/Lights.cpp
+++ src/Subsystems/Lights.cpp
@@ -1,10 +1,33 @@
-/*
- * Lights.cpp
- *
- *  Created on: Oct 23, 2017
- *      Author: VandenRobotics
- */
+#include "Lights.h"
+#include "../RobotMap.h"
+#include "../Commands/SetLights.h"
 
+const char Lights::kSubsystemName[] = "Lights";
 
+std::shared_ptr<Lights> Lights::self;
 
+std::shared_ptr<Lights> Lights::getInstance() {
+	if (! self) {
+		self = std::shared_ptr<Lights>(new Lights);
 
+	}
+	return self;
+}
+
+Lights::Lights() : Subsystem(kSubsystemName),
+mainGlow(RobotMap::kHeimdallGlowID),
+leftGlow(RobotMap::kLeftLightID),
+rightGlow(RobotMap::kRightLightID)
+				{
+			SetGlow(LightState::kLightOn);
+}
+
+void Lights::InitDefaultCommand() {
+	SetDefaultCommand(new SetLights(LightState::kLightOn));
+}
+
+void Lights::SetGlow(LightState value){
+	mainGlow.Set(static_cast<Replay::Value>(value));
+	leftGlow.Set(static_cast<Replay::Value>(value));
+	rightGlow.Set(static_cast<Relay::Value>(value));
+}
