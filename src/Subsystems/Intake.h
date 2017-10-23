a701da76cefcd84c26170736e791a5c080676202
trinitydeloviar <trinityd@vandenrobotics.com>
2017-10-23 15:48:30 -0700

    commit

---
+++ src/Subsystems/Intake.h
@@ -0,0 +1,15 @@
+/*
+ * Intake.h
+ *
+ *  Created on: Oct 23, 2017
+ *      Author: VandenRobotics
+ */
+
+#ifndef SRC_SUBSYSTEMS_INTAKE_H_
+#define SRC_SUBSYSTEMS_INTAKE_H_
+
+
+
+
+
+#endif /* SRC_SUBSYSTEMS_INTAKE_H_ */
