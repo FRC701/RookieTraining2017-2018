1008929327c90078124eb0f4b9fe3118122d9ab5
NicholasKP <unknownwasp@gmail.com>
2017-10-25 15:42:32 -0700

    Added TankDrive command and edited chassis

---
+++ src/Commands/TankDrive.cpp
@@ -0,0 +1,39 @@
+#include "TankDrive.h"
+#include "../Subsystems/Chassis.h"
+#include "../OI.h"
+
+TankDrive::TankDrive() {
+
+
+	Requires(Chassis::getInstance().get());
+}
+
+
+void TankDrive::Initialize() {
+
+}
+
+
+void TankDrive::Execute() {
+	std::shared_ptr<Chassis> chassis = Chassis::getInstance();
+	std::shared_ptr<OI> oi = OI::getInstance();
+
+		chassis->SetTankDrive(oi->kLeftYAxis_ID, oi->kRightYAxis_ID);
+
+}
+
+
+bool TankDrive::IsFinished() {
+		return false;
+}
+
+
+void TankDrive::End() {
+
+}
+
+
+
+void TankDrive::Interrupted() {
+
+}
