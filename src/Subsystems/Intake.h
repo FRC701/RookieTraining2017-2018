ec0046e158c614d2e68f2f4e10f57709cb6cd0ac
tamiavalledor <tamiavalledor@gmail.com>
2017-10-25 16:37:11 -0700

    Fixed

--- src/Subsystems/Intake.h
+++ src/Subsystems/Intake.h
@@ -3,10 +3,10 @@
 
 #include <Commands/Subsystem.h>
 
-class Intake : public frc::Subsytem {
+class Intake : public frc::Subsystem {
 private:
         // It's desirable that everything possible under private except
-        // for methods that implement subsytem capabilities
+        // for methods that implement subsystem capabilities
 
         static const char kSubsytemName[];
         static std::shared_ptr<Intake> self;
