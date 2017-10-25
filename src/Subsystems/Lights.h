dd1cfc16faa43c307b918f5d7737100f6c033837
tamiavalledor <tamiavalledor@gmail.com>
2017-10-25 16:19:12 -0700

    Finished Lights header file

---
+++ src/Subsystems/Lights.h
@@ -0,0 +1,31 @@
+#ifndef Lights_H
+#define Lights_H
+
+#include <Command/Subsytem.h>
+#include <Relay.h>
+
+class Lights : public frc::Subsystem {
+private:
+        // It's desirable that everything possible under private except
+        // for methods that implement subsystem capabilities
+
+  static const char kSubsytemName[];
+  static std::shared_ptr<Lights> self;
+  Lights();
+
+  Relay mainGlow;
+  Relay leftGlow;
+  Relay rightGlow;
+
+public:
+
+  enum LightState{kLightOn = Relay::kOn, kLightOff = Relay::kOff};
+
+  static std::shared_ptr<Lights> getInstance();
+
+        void InitDefaultCommand();
+
+        void SetGlow(LightState value);
+};
+
+#endif // Lights_H
