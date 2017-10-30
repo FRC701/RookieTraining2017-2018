4e592b1acbfd9dcecb1e404499ee22702e9c1b9c
NicholasKP <unknownwasp@gmail.com>
2017-10-30 16:55:51 -0700

    added and finished toggle actuator command

---
+++ src/Commands/ToggleActuator.cpp
@@ -0,0 +1,17 @@
+#include "ToggleActuator.h"
+#include "../Subsystems/Intake.h"
+
+ToggleActuator::ToggleActuator() {
+
+
+	Requires(Intake::getInstance().get());
+}
+
+
+void ToggleActuator::Initialize() {
+
+	Intake::ActuatorValue value
+		= Intake::getInstance()->IsActuatorClosed() ? Intake::kActuatorOpen : Intake::kActuatorClosed;
+	Intake::getInstance()->SetActuator(value);
+
+}
