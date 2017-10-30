cba4c795119415976b31dc1d7dfae39099ad3865
NicholasKP <unknownwasp@gmail.com>
2017-10-30 16:55:06 -0700

    added and finished set actuator command

---
+++ src/Commands/SetActuator.cpp
@@ -0,0 +1,14 @@
+#include "SetActuator.h"
+
+SetActuator::SetActuator(Intake::ActuatorValue value)
+: mValue(value)
+{
+	//comments about using requires
+	//comments about using requires
+	Requires(Intake::getInstance(.get());
+}
+
+//Called just before this Command runs the first time
+void SetActuator::Initialize() {
+		Intake::getInstance()->SetActuator(mValue);
+}
---
+++ src/Commands/SetActuator.h
@@ -0,0 +1,18 @@
+#ifndef SetActuator_H
+#define SetActuator_H
+
+#include "Commands/InstantCommand.h"
+#include "../Subsystems/Intake.h"
+
+class SetActuator : public frc::InstantCommand {
+public:
+		SetActuator(Intake::ActuatorValue value);
+		void Initialize();
+
+
+private:
+		enum Intake::ActuatorValue mValue;
+
+};
+
+#endif //SetActuator_H
