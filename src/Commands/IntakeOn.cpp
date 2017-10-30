e4c62d17dbd1b08273d7072e1b964c68d5bbd4ea
NicholasKP <unknownwasp@gmail.com>
2017-10-30 16:54:22 -0700

    Added and Finished Intake On command

---
+++ src/Commands/IntakeOn.cpp
@@ -0,0 +1,38 @@
+#include "IntakeOn.h"
+#include "../Subsystems/Intake.h"
+
+IntakeOn::IntakeOn(double speed)
+: mSpeed(speed)
+{
+
+
+	Requires(Intake::getInstance().get());
+}
+
+
+void IntakeOn::Initialize() {
+
+}
+
+
+void IntakeOn::Execute() {
+
+		Intake::getInstance()->SetIntake(mSpeed);
+}
+
+
+bool IntakeOn::IsFinished() {
+	return false;
+}
+
+
+void IntakeOn::End() {
+
+}
+
+
+
+void IntakeOn::Interrupted() {
+
+}
+
