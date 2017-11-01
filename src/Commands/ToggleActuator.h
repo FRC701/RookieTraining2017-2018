42146063045ba86cce2afb9f6f6f348373bee606
trinitydeloviar <trinityd@vandenrobotics.com>
2017-11-01 16:54:50 -0700

    commit

---
+++ src/Commands/ToggleActuator.h
@@ -0,0 +1,12 @@
+#ifndef ToggleActuator_H
+#define ToggleActuator_H
+
+#include "Commands/InstantCommand.h"
+
+class ToggleActuator : public frc::InstantCommand {
+public:
+	ToggleActuator();
+	void Initialized();
+
+};
+#endif /* SRC_COMMANDS_TOGGLEACTUATOR_H_ */
