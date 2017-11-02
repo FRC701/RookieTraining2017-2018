7d9b5e12f9b5ad45a8219928e1f4569e70483643
tamiavalledor <tamiavalledor@gmail.com>
2017-11-02 07:28:10 -0700

    Finished SetBrake Command.

---
+++ src/Commands/SetBrake.h
@@ -0,0 +1,16 @@
+#ifndef SetBrake_H
+#define SetBrake_H
+
+#include "Commands/InstantCommand.h"
+#include "../Subsystems/Elevator.h"
+
+class SetBrake : public InstantCommand {
+public:
+		SetBrake(Elevator::BrakeValue value);
+		void Initialize();
+
+private:
+		enum Elevator::BrakeValuie mValue
+};
+
+#endif // SetBrake_H
