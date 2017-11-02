9e675c1fd4c052cd30f543a89b9f1d0deb4968f1
tamiavalledor <tamiavalledor@gmail.com>
2017-11-01 19:23:12 -0700

    Finished ElevatorOn Command.

---
+++ src/Commands/ElevatorOn.h
@@ -0,0 +1,19 @@
+#ifndef ElevatorOn_H
+#define ELevatorOn_H
+
+#include "Commands/Command.h"
+
+class ElevatorOn : public frc::Command {
+public:
+		ElevatorOn(double speed);
+		void Initialize();
+		void Execute();
+		bool IsFinished();
+		void End();
+		void Interrupted();
+
+private:
+		double mSpeed;
+};
+
+#endif // ElevatorOn.H
