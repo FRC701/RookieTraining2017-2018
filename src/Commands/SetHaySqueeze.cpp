ae4ed71f48f7d8e3f7411919cdf20467d5dc4208
NicholasKP <unknownwasp@gmail.com>
2017-11-03 16:06:53 -0700

    added set hay squeeze command

---
+++ src/Commands/SetHaySqueeze.cpp
@@ -0,0 +1,16 @@
+#include "SetHaySqueeze.h"
+
+
+SetHaySqueeze::SetHaySqueeze(Elevator::HaySqueezeValue value)
+: mValue(value) {
+	//sssssssstuuuuuffffffffffffff
+	//stuff
+	Requires(Elevator::getInstance().get());
+}
+
+//TODO delete my project jk dont
+void SetHaySqueeze::Initialize() {
+
+	Elevator::getInstance()->SetHaySqueeze(mValue);
+
+}
