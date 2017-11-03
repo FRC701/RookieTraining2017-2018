8770a1859ccb12467ccb8dfc2b4b03fc7f387738
NicholasKP <unknownwasp@gmail.com>
2017-11-03 15:35:17 -0700

    Completed Elevator subsystem

--- src/Subsystems/Elevator.h
+++ src/Subsystems/Elevator.h
@@ -2,11 +2,18 @@
 #define Elevator_H
 
 #include <Commands/Subsystem.h>
+#include <CANTalon.h>
+#include <DoubleSolenoid.h>
 
 class Elevator : public Subsystem {
 private:
 	// It's desirable that everything possible under private except
 	// for methods that implement subsystem capabilities
+
+	CANTalon leftSpool;
+	CANTalon rightSpool;
+	DoubleSolenoid haySqueeze;
+	DoubleSolenoid brake;
 
 	static const char kSubsystemName[];
 	static std::shared_ptr<Elevator> self;
@@ -14,9 +21,21 @@ private:
 
 public:
 
+	enum BrakeValue {kBrakeOn = DoubleSolenoid::kForward, kBrakeOff = DoubleSolenoid::kReverse };
+	enum HaySqueezeValue {kOpen = DoubleSolenoid::kForward, kClose = DoubleSolenoid::kReverse };
+
 	static std::shared_ptr<Elevator> getInstance();
 
 	void InitDefaultCommand();
+
+	void SetBrake(BrakeValue value);
+	bool IsBrakeOn() const;
+
+	bool IsHaySqueezepen() const;
+	void SetHaySqueeze(HaySqueezeValue value);
+
+	void SetSpool (double speed);
+
 };
 
 #endif  // Elevator_H
