7ef1fc23e7fc7047ea2ef2e614386dabcc596632
tamiavalledor <tamiavalledor@gmail.com>
2017-11-05 15:16:32 -0800

    Update Intake Subsystem.

--- src/Subsystems/Intake.cpp
+++ src/Subsystems/Intake.cpp
@@ -1,5 +1,6 @@
 #include "Intake.h"
 #include "../RobotMap.h"
+#include "../Commands/IntakeOn.h"
 
 const char Intake::kSubsytemName[] = "Intake";
 
@@ -12,15 +13,46 @@ std::shared_ptr<Intake> Intake::getInstance() {
   return self;
 }
 
-Intake::Intake() : Subsystem(kSubsytemName) {
+Intake::Intake() : Subsystem(kSubsytemName)
+				leftSpinner(RobotMap::kLeftSpinnerID),
+				rightSpinner(RobotMap::kRightSpinnerID),
+				actuator(RobotMap::kActuatorF, RobotMap::kActuatorR)
 
+{
+				SetupIntake();
 }
 
 void Intake::InitDefaultCommand() {
         // Set the default command for a subsystem here.
         // SetDefaultCommand(new MySpecialCommand());
+		        SetDefaultCommand(new IntakeOn(0.0));
 }
 
 // Put methods for controlling this subsystem
 // here. Call these from Commands.
+
+bool Intake::IsActuatorClosed() const{
+
+		return actuator.Get() == static_cast<DoubleSolenoid::Value>(kActuatorClosed);
+
+}
+
+void Intake::SetActuator(ActuatorValue value){
+
+				actuator.Set(static_cast<DoubleSolenoid::Value>(value));
+
+}
+
+void Intake::SetIntake(double speed){
+
+				leftSpinner.Set(speed);
+				rightSpinner.Set(speed);
+
+}
+
+void Intake::SetupIntake() {
+				leftSpinner.SetControlMode(CANTalon::kVoltage);
+				rightSpinner.SetControlMode(CANTalon::kVOltage)
+
+}
 
