ee1ca0ad4b3b0bb8389a5c6bfd0701ec8897430a
tamiavalledor <tamiavalledor@gmail.com>
2017-11-05 16:09:18 -0800

    Fixups

--- src/Commands/SetBrake.cpp
+++ src/Commands/SetBrake.cpp
@@ -11,5 +11,4 @@ SetBrake::SetBrake(Elevator::BrakeValue value)
 void SetBrake::Initialize() {
 
 		Elevator::getInstance()->SetBrake(mValue);
-
 }
--- src/Commands/SetBrake.h
+++ src/Commands/SetBrake.h
@@ -10,7 +10,7 @@ public:
 		void Initialize();
 
 private:
-		enum Elevator::BrakeValuie mValue;
+		enum Elevator::BrakeValue mValue;
 };
 
 #endif // SetBrake_H
