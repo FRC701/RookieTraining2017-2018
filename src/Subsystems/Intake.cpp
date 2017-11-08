6a7554e77d6084b86d14b4d676d65a7a3cfe2e13
trinitydeloviar <trinityd@vandenrobotics.com>
2017-11-08 15:25:39 -0800

    commit

--- src/Subsystems/Intake.cpp
+++ src/Subsystems/Intake.cpp
@@ -1,10 +1,49 @@
-/*
- * Intake.cpp
- *
- *  Created on: Oct 23, 2017
- *      Author: VandenRobotics
- */
+#include "Intake.h"
+#include "../RobotMap.h"
+#include "../Commands/IntakeOn.h"
 
+const char Intake::kSubsystemName[] = "Intake";
 
+std::shared_ptr<Intake> Intake::self;
 
+std::shared_ptr<Intake> Intake::getInstance() {
+	if(! self) {
+		self = std::shared_ptr<Intake>(new Intake);
+	}
+	return self;
+}
 
+Intake::Intake() : Subsystem(kSubsystemName),
+		leftSpinner(RobotMap::kLeftSpinnerID),
+		rightSpinner(RobotMap::kRightSpinnerID),
+		actuator(RobotMap::kActuatorF, RobotMap::kActuatorR)
+
+{
+	SetupIntake();
+}
+
+void Intake::InitDefaultCommand() {
+	SetDefaultCommand(new IntakeOn(0.0));
+}
+
+bool Intake::IsActuatorClosed() const{
+
+	return actuator.Get() == static_cast<DoubleSolenoid::Value>(kActuatorClosed);
+
+}
+
+void Intake::SetActuator(ActuatorValue value){
+	actuator.Set(static_cast<DoubleSolenoid::Value>(value));
+
+}
+
+void Intake::SetIntake(double speed){
+	leftSpinner.Set(speed);
+	rightSpinner.Set(speed);
+
+}
+
+void Intake::SetupIntake() {
+	leftSpinner.SetControlMode(CANTalon::kVoltage);
+	rightSpinner.SetControlMode(CANTalon::kVoltage);
+}
