2ac5c9ada667a39e73784c4be0e868cae2ea9075
trinitydeloviar <trinityd@vandenrobotics.com>
2017-11-08 16:48:04 -0800

    commit

--- src/Subsystems/Elevator.cpp
+++ src/Subsystems/Elevator.cpp
@@ -12,12 +12,32 @@ std::shared_ptr<Elevator> Elevator::getInstance() {
 	return self;
 }
 
-Elevator::Elevator() : Subsystem(kSubsystemName) {
+Elevator::Elevator() : Subsystem(kSubsystemName),
+		leftSpool(RobotMap::kLeftSpoolID),
+		rightSpool(RobotMap::kRightSpoolID),
+		haySqueeze(RobotMap::kHaySqueezeF, RobotMap::kHaySqueezeR),
+		brake(RobotMap::kBrakeF,RobotMap::kBrakeR)
+{
 
 }
 
+
 void Elevator::InitDefaultCommand() {
 
+}
+
+void Elevator::SetSpool(double speed){
+	leftSpool.Set(speed);
+	rightSpool.Set(speed);
+
+}
+
+bool Elevator::IsBrakeOn() const{
+	return brake.Get() == static_cast<DoubleSolenoid::Value>(kOpen);
+}
+
+void Elevator::SetHaySqueeze(HaySqueezeValue value){
+	haySqueeze.Set(static_cast<DoubleSolenoid::Value>(value));
 }
 
 
