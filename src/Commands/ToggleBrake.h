ce05d8071ab10659e94aca38b83934d3f629d3dd
trinitydeloviar <trinityd@vandenrobotics.com>
2017-11-08 16:46:59 -0800

    commit

---
+++ src/Commands/ToggleBrake.h
@@ -0,0 +1,18 @@
+#ifndef ToggleBrake_H
+#define ToggleBrake_H
+
+#include "Commands/InstantCommand.h"
+
+class ToggleBrake : public frc::InstantCommand {
+public:
+	ToggleBrake();
+	void Initialize();
+};
+
+
+
+
+
+
+
+#endif /* SRC_COMMANDS_TOGGLEBRAKE_H_ */
