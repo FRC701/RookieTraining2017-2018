df497c65a42655b9075668406312793c7eba6792
trinitydeloviar <trinityd@vandenrobotics.com>
2017-11-08 16:47:54 -0800

    commit

--- src/Subsystems/Elevator.h
+++ src/Subsystems/Elevator.h
@@ -1,19 +1,36 @@
-#ifndef ELEVATOR_H
-#define ELEVATOR_H
+#ifndef Elevator_H
+#define Elevator_H
 
 #include <Commands/Subsystem.h>
+#include <CANTalon.h>
+#include <DoubleSolenoid.h>
 
 class Elevator : public frc::Subsystem {
 private:
+	CANTalon leftSpool;
+	CANTalon rightSpool;
+	DoubleSolenoid haySqueeze;
+	DoubleSolenoid brake;
+
 	static const char kSubsystemName[];
 	static std::shared_ptr<Elevator> self;
 	Elevator();
 
 public:
+	enum BrakeValue {kBrakeOn = DoubleSolenoid::kForward, kBrakeOff = DoubleSolenoid::kReverse };
+	enum HaySqueezeValue {kOpen = DoubleSolenoid::kForward, kClose = DoubleSolenoid::kReverse };
 
 	static std::shared_ptr<Elevator> getInstance();
 
 	void InitDefaultCommand();
+
+	void SetBrake(BrakeValue value);
+	bool IsBrakeOn() const;
+
+	bool IsHaySqueezepen() const;
+	void SetHaySqueeze(HaySqueezeValue value);
+
+	void SetSpool(double speed);
 
 };
 
