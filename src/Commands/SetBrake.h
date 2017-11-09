480fdc21f4ce41450688510e40c805f2a3605eaf
trinitydeloviar <trinityd@vandenrobotics.com>
2017-11-08 16:47:25 -0800

    commit

---
+++ src/Commands/SetBrake.h
@@ -0,0 +1,17 @@
+#ifndef SetBrake_H
+#define SetBrake_H
+
+#include"Commands/InstantCommand.h"
+#include"../Subsystems/Elevator.h"
+
+class SetBrake : public InstantCommand {
+public:
+	SetBrake(Elevator::BrakeValue value);
+	void Initialize();
+
+private:
+	enum Elevator::BrakeValue mValue;
+
+};
+
+#endif /* SRC_COMMANDS_SETBRAKE_H_ */
